2098c0c76c3e49c554dd00edbafe49c38ce0e6c2
YeisonPetit <yeisonpetit2@gmail.com>
2025-08-18 21:52:26 -0400

    Fix PokemonsCart import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import PokemonsCart from "./components/pokemonsCart";
+import PokemonsCart from "./components/PokemonsCart";
 
 function App() {
   const [pokemonData, setPokemon] = useState([])
